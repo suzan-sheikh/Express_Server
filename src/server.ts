f0032b11f36aa4c0688adb7ff33352eea2f3215b
suzan-sheikh <mdsuzanskh@gmail.com>
2026-05-19 13:10:24 +0600

    use express midleware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
-import express from "express";
-const app = express();
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+const app: Application = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json())
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Express Server",
+    "Author ": "EccTechBD",
+  });
+  //res.send("Hello World!");
 });
+
+app.post('/', async(req: Request, res: Response)=> {
+    console.log(req.body);
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
